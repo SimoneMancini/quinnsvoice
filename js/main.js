7deff1418e3335cde283fcc0552f6e0f7d295d67
Christoher Kennedy <kennedysgarage@gmail.com>
2013-02-16 00:30:23 -0500

    making the text flip in the intro

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
 
+  // http://stackoverflow.com/questions/8965651/cycle-through-divs
+  // http://jsfiddle.net/eFjnU/
+  var divs = $('strong[id^="word-"]').hide(),
+      i = 0;
+
+  (function cycle() {
+
+      divs.eq(i).fadeIn(400)
+                .delay(1500)
+                .fadeOut(400, cycle);
+
+      i = ++i % divs.length;
+
+  })();
+
+});
